4a7842360d4e81677bcd183dbc9cbebb7f451405
Spencer Carter <scmountain@gmail.com>
2016-12-14 14:35:17 -0700

    adds all bare files

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -6,7 +6,7 @@ require './lib/offset'
 class OffsetTests< MiniTest::Test
   def setup
     @numbers = Offsets.new
-    @insert = @numbers.insert(120816)
+    @insert = @numbers.insert(123456)
   end
 
   def test_offset
@@ -16,31 +16,32 @@ class OffsetTests< MiniTest::Test
 
   def test_insert_returns_split
     @numbers
-    # assert_equal ["3", "9","3", "6"], @insert
+    assert_equal ["3", "9","3", "6"], @insert
   end
 
   def test_offset_a
     @numbers
     @insert
-    # assert_equal 3, @numbers.set_offset_a
+    assert_equal 3, @numbers.set_offset_a
   end
 
   def test_offset_b
     @numbers
     @insert
-    # assert_equal 9, @numbers.set_offset_b
+    assert_equal 9, @numbers.set_offset_b
   end
 
   def test_offset_c
     @numbers
     @insert
-    # assert_equal 3, @numbers.set_offset_c
+    assert_equal 3, @numbers.set_offset_c
   end
 
   def test_offset_d
     @numbers
     @insert
-    # assert_equal 6, @numbers.set_offset_dy
+    binding.pry
+    assert_equal 6, @numbers.set_offset_d
   end
 
 end
