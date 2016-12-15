845d884b62e087c4efbea7b3a7f4ae729f0ca270
Greg Taylor <gregtaylor@Gregs-MBP.T-mobile.com>
2016-12-14 20:08:50 -0700

    working copy of enigma decrypt

--- test/enigma_decrypt_char_map_test.rb
+++ test/enigma_decrypt_char_map_test.rb
@@ -4,13 +4,16 @@ require "./lib/enigma_decrypt_char_map"
 
 class EnigmaDecryptCharMapTest < Minitest::Test
 
-@@map = EnigmaDecryptCharMap.new
+  def setup
+     @map = EnigmaDecryptCharMap.new
+  end
+
 
   def test_if_it_exists
-    assert_equal false,@@map.nil?
+    assert_equal false,@map.nil?
   end
 
   def test_if_it_is_a_hash
-    assert_instance_of Hash, @@map.char_map
+    assert_instance_of Hash, @map.decrypt
   end
 end
