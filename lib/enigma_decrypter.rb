82a45f1c9e5c1055cae94c3f8629858ea9e24713
Greg Taylor <gregtaylor@Gregs-MBP.T-mobile.com>
2016-12-14 20:26:51 -0700

    working copy of enigma_decrypt_char_map and test

---
+++ lib/enigma_decrypter.rb
@@ -0,0 +1,67 @@
+require "./lib/offset"
+require "./lib/enigma_decrypt_char_map"
+require 'pry'
+class EnigmaDecrypter
+
+
+
+  def code(x)
+    if x == []
+      @key = 5.times.map{rand(9)}.to_a
+    else @key = x
+    end
+  end
+
+  def math_a
+    @math_a = (@key[0].to_s + @key[1].to_s).to_i
+  end
+
+  def math_b
+    @math_b = (@key[1].to_s + @key[2].to_s).to_i
+  end
+
+  def math_c
+    @math_c = (@key[2].to_s + @key[3].to_s).to_i
+  end
+
+  def math_d
+    @math_d = (@key[3].to_s + @key[4].to_s).to_i
+  end
+
+  def offset_1
+    @something1 = Offsets.new.set_offset_a
+    offset_1 = @something1
+    @key_a = offset_1 + math_a
+  end
+
+  def offset_2
+    @something2 = Offsets.new.set_offset_b
+    offset_2 = @something2
+    @key_b = offset_2 + math_b
+  end
+
+  def offset_3
+    @something3 = Offsets.new.set_offset_c
+    offset_3 = @something3
+    @key_c = offset_3 + math_c
+  end
+
+  def offset_4
+  @something4 = Offsets.new.set_offset_d
+  offset_4 = @something4
+    @key_d = offset_4 + math_d
+  end
+
+  def decrypt(letter)
+    @start=EnigmaDecryptCharMap.new
+    decrypt_char_map = @start.decrypt_char_map
+    @first_step = decrypt_char_map[letter].to_i + @key_a
+    while @first_step > 39
+      @first_step = @first_step - 39
+    end
+      @final_step = decrypt_char_map.key(@first_step)
+  end
+
+
+
+end
---
+++ test/enigma_decrypter_test.rb
@@ -0,0 +1,91 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/enigma_decrypter"
+require "./lib/enigma_decrypt_char_map"
+
+class EnigmaDecrypterTest < MiniTest::Test
+
+  def setup
+    @rotation = EnigmaDecrypter.new
+  end
+
+  def test_there_is_a_key
+  assert_equal false,  @rotation.code([]).nil?
+  end
+
+  def test_we_enter_key
+    assert_equal [4,7,6,1,9],@rotation.code([4,7,6,1,9])
+  end
+
+  def test_the_key_is_an_array
+    assert_instance_of Array,@rotation.code([4,7,6,1,9])
+  end
+
+  def test_we_can_get_a_random_key
+    assert_equal false, @rotation.code([]).nil?
+  end
+
+  def test_random_key_is_an_array
+    assert_instance_of Array,@rotation.code([])
+  end
+
+  def test_we_get_first_part_of_key
+    @rotation.code([4,7,6,1,9])
+    assert_equal 47, @rotation.math_a
+  end
+
+  def test_we_get_second_part_of_key
+    @rotation.code([4,7,6,1,9])
+    assert_equal 76, @rotation.math_b
+  end
+
+  def test_we_get_third_part_of_key
+    @rotation.code([4,7,6,1,9])
+    assert_equal 61, @rotation.math_c
+  end
+
+  def test_we_get_fourth_part_of_key
+    @rotation.code([4,7,6,1,9])
+    assert_equal 19, @rotation.math_d
+  end
+
+  def test_we_can_get_final_key_a
+    @rotation.code([4,7,6,1,9])
+    @rotation.math_a
+    assert_equal 52, @rotation.offset_1
+  end
+
+  def test_we_can_get_final_key_b
+    @rotation.code([4,7,6,1,9])
+    @rotation.math_b
+    assert_equal 82, @rotation.offset_2
+  end
+
+  def test_we_can_get_final_key_c
+    @rotation.code([4,7,6,1,9])
+    @rotation.math_c
+    assert_equal 70, @rotation.offset_3
+  end
+
+  def test_we_can_get_final_key_d
+    @rotation.code([4,7,6,1,9])
+    @rotation.math_d
+    assert_equal 24, @rotation.offset_4
+  end
+
+  def test_can_we_encrypt_a_letter
+    @rotation.code([4,1,5,2,1])
+    @rotation.offset_1
+    assert_equal "3",@rotation.decrypt("w")
+  end
+
+  def test_can_we_encrypt_a_2nd_letter
+    @rotation.code([4,1,5,2,1])
+    @rotation.offset_2
+    assert_equal "3",@rotation.decrypt("w")
+  end
+
+
+
+
+end
